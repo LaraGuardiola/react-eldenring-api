1c510de3d0fdf517ed72e58bbcfef2fe4c342b5f
Sergio <webdev.slara@gmail.com>
2022-04-18 19:21:35 +0200

    added more styling

--- src/components/BossDetail.js
+++ src/components/BossDetail.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import '../css/BossDetail.css';
 
-const BossDetail = ({boss}) => {
-    if(boss.name === 'Abductor Virgins'){
+const BossDetail = ({ boss }) => {
+    if (boss.name === 'Abductor Virgins') {
         return (
             <div className="boss-detail">
                 <h1>{boss.name}</h1>
                 <img src="https://eldenring.wiki.fextralife.com/file/Elden-Ring/maxresdefault.jpg" alt={boss.description} />
                 <p>{boss.description}</p>
-                <p>Location: {boss.location}</p>
-                <p>Drops: {boss.drops.map((drop,index) => {
-                    if(boss.drops.length === index + 1) {
-                        return `${drop}.`
-                    }
-                    return `${drop}, `
-                })}
-                </p>
+                <div className="description">
+                    <p>Location: {boss.location}</p>
+                    <p>Drops: {boss.drops.map((drop, index) => {
+                        if (boss.drops.length === index + 1) {
+                            return `${drop}.`
+                        }
+                        return `${drop}, `
+                    })}
+                    </p>
+                </div>
+
             </div>
         )
     }
@@ -24,14 +27,16 @@ const BossDetail = ({boss}) => {
             <h1>{boss.name}</h1>
             <img src={boss.image} alt={boss.description} />
             <p>{boss.description}</p>
-            <p>Location: {boss.location}</p>
-            <p>Drops: {boss.drops.map((drop,index) => {
-                if(boss.drops.length === index + 1) {
-                    return `${drop}.`
-                }
-                return `${drop}, `
-            })}
-            </p>
+            <div className="description">
+                <p>Location: {boss.location}</p>
+                <p>Drops: {boss.drops.map((drop, index) => {
+                    if (boss.drops.length === index + 1) {
+                        return `${drop}.`
+                    }
+                    return `${drop}, `
+                })}
+                </p>
+            </div>
         </div>
     )
 }
